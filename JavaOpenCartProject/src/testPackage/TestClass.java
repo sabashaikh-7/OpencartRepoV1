60aa7d1124e7a58c134dba76221aab52bb3e6cfd
Amar Waghmare <amarwaghmare573@gmail.com>
2023-03-08 09:08:15 +0530

    First commit

---
+++ JavaOpenCartProject/src/testPackage/TestClass.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("TestBase class");
+		System.out.println("pom.xml");
+		System.out.println("Util");
+
+	}
+
+}
